2051c77b34f95e792ce376bdf2bbef5381a851e7
pet-petrov <63002779+pet-petrov@users.noreply.github.com>
2020-04-02 17:28:06 +0300

    Final!

--- Exams/december2019_3.js
+++ Exams/december2019_3.js
@@ -1,5 +1,6 @@
 function solve(input) {
     input.pop()
+    let count = 0
     let emails = {}
     for (const line of input) {
         let [command, user, message] = line.split('->');
@@ -11,65 +12,83 @@ function solve(input) {
                 sendMail(user, message)
                 break;
             case 'Delete':
-
+                deleteUser(user)
                 break;
 
         }
     }
     function addUser(userName) {
         if (emails.hasOwnProperty(userName)) {
-            console.log(`"${userName} is already registered`);
+            console.log(`${userName} is already registered`);
         }
         else {
             emails[userName] = [];
+            count++;
         }
-        console.log(emails);
-        
     }
 
     function sendMail(userName, mail) {
         emails[userName].push(mail);
+
     }
 
     function deleteUser(userName) {
         if (emails[userName]) {
-            delete emails[username];
+            delete emails[userName];
+            count--;
         }
         else {
             console.log(`${userName} not found!`)
         }
     }
-    let usersArray=Object.entries(emails);
-    let sortedUsers=usersArray.sort(sortUsers);
+    let usersArray = Object.entries(emails);
+    let sortedUsers = usersArray.sort(sortUsers);
 
-    function sortUsers(userA,userB){
-       let userNameA=userA[0];
-       let emailsA=userA[1];
-       let lengthA=emailsA.length
-       let userNameB=userB[0];
-       let emailsB=userB[1];
-       let lengthB=emailsB.length
-       if(lengthA==lengthB){
-           return userNameA.localeCompare(userNameB);
-       }
-       else{
-           return lengthB-lengthA;
-       }
+    function sortUsers(userA, userB) {
+        let userNameA = userA[0];
+        let emailsA = userA[1];
+        let lengthA = emailsA.length
+        let userNameB = userB[0];
+        let emailsB = userB[1];
+        let lengthB = emailsB.length
+        if (lengthA == lengthB) {
+            return userNameA.localeCompare(userNameB);
+        }
+        else {
+            return lengthB - lengthA;
+        }
     }
-     console.log(sortedUsers);
+    function output(users) {
+
+        for (const user of users) {
+            console.log(user[0]);
+            let emails = user[1];
+            for (let i = 0; i < emails.length; i++) {
+                console.log(` - ${emails[i]}`)
+            }
+        }
+
+    }
+    console.log(`Users count: ${count}`);
+
+
+    output(sortedUsers)
 }
 
 solve(
     [
-        'Add->Mike',
-        'Add->George',
-        'Send->George->Hello World',
-        'Send->George->Some random test mail',
-        'Send->Mike->Hello, do you want to meet up tomorrow?',
-        'Send->George->It would be a pleasure',
-        'Send->Mike->Another random test mail',
-        'Send->Mike->Hello, do you want to meet up tomorrow?',
-        'Statistics'
+        "Add->George",
+        "Send->George->Hello World",
+        "Add->Mike",
+        "Send->George->Your loan is overdue",
+        "Add->Mike",
+       " Send->Mike->Hello, do you want to meet up tomorrow?",
+        "Delete->Peter",
+        "Send->George->I'm busy",
+        "Send->Mike->Another random test mail",
+        "Delete->George",
+        "Statistics"
+
     ]
 
 )
